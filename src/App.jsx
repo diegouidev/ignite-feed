3e44f0f330ec83fbade8f7cbd206653d329c884b
diegouidev <diegocoelho44@gmail.com>
2022-12-06 11:08:12 -0300

    adding css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Header } from './components/Header'
+import { Post } from './Post'
 
-function App() {
+export function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <Post
+        author="Diego Colelho"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos et esse est corrupti quaerat quas labore voluptas laudantium iste nam ipsum delectus enim quisquam impedit explicabo, ullam quod eos quasi."
+      />
+     <Post
+        author="Rene Coutinho"
+        content="Lorem ipsum dolor sit."
+      />
+      <Header />
+    </>
   )
 }
-
-export default App

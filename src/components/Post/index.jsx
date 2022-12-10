a320df633f9ef0d0403ba02e2c01ba0c8eecdaba
diegouidev <diegocoelho44@gmail.com>
2022-12-09 21:04:14 -0300

    avatar styling

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../Avatar'
 import { Comment } from '../Comments'
 import styles from './Post.module.css'
 
@@ -6,9 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/diegouidev.png" />
+          <Avatar hasBorder src='https://github.com/diegouidev.png' />
           <div className={styles.authorInfo}>
             <strong>Diego Coelho</strong>
             <span>Web Developer</span>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from '../Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -9,9 +10,7 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/diegouidev.png" />
+        <Avatar src='https://github.com/diegouidev.png' />
 
         <strong>Diego Coelho</strong>
         <span>Web Developer</span>
